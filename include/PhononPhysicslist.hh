5f1605d11c466f89e7b7c58990857a818d1d647f
Israel-Tanjiro <96498069+Israel-Tanjiro@users.noreply.github.com>
2022-02-02 23:20:19 -0600

    Update PhononPhysicslist.hh

--- include/PhononPhysicslist.hh
+++ include/PhononPhysicslist.hh
@@ -1,1 +1,17 @@
+#ifndef PHONONPHYSICSLIST_HH
+#define PHONONPHYSICSLIST_HH
 
+#include "G4VModularPhysicsList.hh"
+#include "G4EmStandardPhysics.hh"
+#include "G4OpticalPhysics.hh"
+#include "G4RadioactiveDecayPhysics.hh"
+#include "G4DecayPhysics.hh"
+
+class PhononPhysicslist : public G4VModularPhysicsList
+{
+public:
+    PhononPhysicslist();
+    ~PhononPhysicslist();
+};
+
+#endif
